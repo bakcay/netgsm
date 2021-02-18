d9e24e78316f63d5d82eaadb5b8bfbf0a0aa8f65
bunyaminakcay <Bunyamin@81>
2021-02-18 18:10:58 +0300

    struct improvement

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-
-
-Laravel netgsm Package
+## Laravel netgsm Package
 ===
 
 
--- src/NetGsm.php
+++ src/NetGsm.php
@@ -21,6 +21,12 @@ class NetGsm
         $this->content = $message;
     }
 
+    public static function sendMessage($message,$number){
+       self::setNumber($number);
+        self::setMessage($message);
+       return  self::send();
+    }
+
     public function setMessage($message){
         $this->content = $message;
         return $this;
@@ -33,29 +39,28 @@ class NetGsm
 
     public function send() {
 
-            $message = '<?xml version="1.0" encoding="iso-8859-9"?>
-                        <mainbody>
-                            <header>
-                                <company>NETGSM</company>
-                                <usercode>' . $this->username . '</usercode>
-                                <password>' . $this->password . '</password>
-                                <startdate></startdate>
-                                <stopdate></stopdate>
-                                <type>1:n</type>
-                                <msgheader>' . $this->header . '</msgheader>
-                            </header>
-                            <body>
-                                <msg><![CDATA[' . $this->content . ']]></msg>
-                                <no>90' . $this->gsmNumber . '</no>
-                            </body>
-                        </mainbody>';
-
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, 'http://api.netgsm.com.tr/xmlbulkhttppost.asp');
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: text/xml"]);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $message);
-            return curl_exec($ch);
+        $message = '<?xml version="1.0" encoding="iso-8859-9"?>
+                    <mainbody>
+                        <header>
+                            <company>NETGSM</company>
+                            <usercode>' . $this->username . '</usercode>
+                            <password>' . $this->password . '</password>
+                            <startdate></startdate>
+                            <stopdate></stopdate>
+                            <type>1:n</type>
+                            <msgheader>' . $this->header . '</msgheader>
+                        </header>
+                        <body>
+                            <msg><![CDATA[' . $this->content . ']]></msg>
+                            <no>90' . $this->gsmNumber . '</no>
+                        </body>
+                    </mainbody>';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'http://api.netgsm.com.tr/xmlbulkhttppost.asp');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: text/xml"]);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $message);
+        return curl_exec($ch);
 
     }
 
